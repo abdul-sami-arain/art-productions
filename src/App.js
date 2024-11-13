5c4113b680f6ff2209ff9c0b20cc282bd9cb0122
abdul-sami-arain <abdulsamiarain82@gmail.com>
2024-11-13 22:47:03 +0500

    sixth

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import AllRoutes from './utils/routes';
 import  AOS  from 'aos';
 import 'aos/dist/aos.css';
 import bg1 from "../src/Assets/bg1.png"
+import contact_img from "../src/Assets/covers/contact.png";
+import about_img from "../src/Assets/covers/about.png";
+import portfolio_img from "../src/Assets/covers/portfolio.png";
+import services_img from "../src/Assets/covers/services.png";
 import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
@@ -43,7 +47,7 @@ const MainContaint = () => {
       welcomText: 'Welcome To Art Production',
       mainHeading: 'About Us',
       heroDesc: '',
-      img:bg1
+      img:about_img
     },
     '/our-projects': {
       welcomText: 'Welcome To Art Production',
@@ -51,7 +55,7 @@ const MainContaint = () => {
       heroDesc: `From coming up with creative concepts to delivering outstanding campaigns, 
                  we're your friendly, fun-loving crew ready to turn your project dreams into 
                  reality Where every move matters.`,
-      img:bg1
+      img:portfolio_img
     },
     '/services': {
       welcomText: 'Welcome To Art Production',
@@ -59,7 +63,7 @@ const MainContaint = () => {
       heroDesc: `Through our long experience in this field, we were involved in many sections 
                  of video production. We don’t create only, instead, we try always to invent 
                  and be creative, we think out of the box, that’s why we are the best.`,
-      img:bg1
+      img:services_img
     },
     '/contact-us': {
       welcomText: 'Welcome To Art Production',
@@ -67,7 +71,7 @@ const MainContaint = () => {
       heroDesc: `We love hearing from you! Whether you're ready to discuss your next project, 
       have a question, or just want to say hello, we're all ears. Our team is here to make your 
       experience exceptional. Drop us a message, and let's start a conversation.`,
-      img:bg1
+      img:contact_img
     }
   }
 
